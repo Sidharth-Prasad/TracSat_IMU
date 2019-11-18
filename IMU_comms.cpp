10d9c837f5d9cc94199154e22fed1f83f399eb3d
Sidharth Prasad <sidharth@bhatni.com>
2019-11-18 04:49:01 -0500

    Added Boost and have a test file

--- IMU_comms.cpp
+++ IMU_comms.cpp
@@ -1,8 +1,29 @@
-#include <stdio.h>
-#include <iostream.h>
+#include <boost_1_71_0/boost/asio/serial_port.hpp> 
+#include <boost/asio.hpp> 
+ 
+using namespace boost;
+ 
+char read_char() {
+	asio::io_service io;
+	asio::serial_port port(io, "/dev/ttyS0");
+ 
+	port.set_option(asio::serial_port_base::baud_rate(115200));
+ 
+	char c;
+ 
+	// Read 1 character into c, this will block
+	// forever if no character arrives.
+	asio::read(port, asio::buffer(&c,1));
+ 
+	port.close();
+ 
+	return c;
+}
 
-
-int mainOP {
-    cout << "hello Warld";
-    return 0;
+int main() {
+    while(true)
+    {
+        printf(read_char());
+        printf("\n")   
+    }
 }
